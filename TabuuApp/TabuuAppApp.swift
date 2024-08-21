230f49a97ce5a34c3fb8cf513d3eb07e104171d6
Mehmet Akif Kaymak <m.akif.kaymak@hotmail.com>
2024-08-21 13:45:33 +0300

    Initial Commit

---
+++ TabuuApp/TabuuAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabuuAppApp.swift
+//  TabuuApp
+//
+//  Created by Mehmet Akif Kaymak on 21.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct TabuuAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
